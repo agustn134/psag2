3a56d2a244e7cf3ad316f3e78b8a6abf9a5c3ce3
agustn134 <agustinparralopez1@gmail.com>
2024-10-09 09:39:23 -0600

    Front End Informacion Visual
    Diseño de componente

--- client/src/app/components/visual-information/visual-information.component.ts
+++ client/src/app/components/visual-information/visual-information.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { YtService } from '../../services/yt.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-visual-information',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrl: './visual-information.component.css'
 })
 export class VisualInformationComponent {
+  videos: any[] | undefined;
+  videoUrl: SafeResourceUrl;
+  searchTerm: string = '';
 
+
+  constructor(private ytService: YtService, private sanitizer: DomSanitizer) {
+    this.videoUrl = '';
+  }
+
+  ngOnInit(): void {
+    this.searchVideos();
+    const body = document.querySelector('body');
+  }
+
+
+  getVideoUrl(videoId: string): SafeResourceUrl {
+    if (videoId) {
+      const protocol: string = 'https';
+      const url = protocol +'://www.youtube.com/embed/' + videoId;
+      return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+    }
+    return '';
+  }
+
+  searchVideos() {
+      this.ytService.getVideos(this.searchTerm)
+      .subscribe((data: any) => {
+        this.videos = data.items;
+      });
+  }
 }
