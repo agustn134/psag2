fb88bbfe5e09b851a2e3a473a4e733dadae9bfd8
agustn134 <agustinparralopez1@gmail.com>
2024-11-23 18:09:40 -0600

    Cambio de direccion de database

--- server/build/keys.js
+++ server/build/keys.js
@@ -2,10 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'bd_psga6'
+        // host: 'localhost',
+        // user: 'root',
+        // password: '',
+        // database: 'bd_psga6'
+        host: 'sql3.freesqldatabase.com',
+        user: 'sql3746356',
+        password: 'Eb42zTrQXh',
+        database: 'sql3746356',
+        port: 3306 // Opcional si ya es el puerto predeterminado
     },
     jwtSecret: '1234567890'
 };
--- server/src/keys.ts
+++ server/src/keys.ts
@@ -1,9 +1,14 @@
 export default{
     database:{
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'bd_psga6'
+        // host: 'localhost',
+        // user: 'root',
+        // password: '',
+        // database: 'bd_psga6'
+        host: 'sql3.freesqldatabase.com',
+        user: 'sql3746356',
+        password: 'Eb42zTrQXh',
+        database: 'sql3746356',
+        port: 3306 // Opcional si ya es el puerto predeterminado
     },
     jwtSecret: '1234567890'
 
