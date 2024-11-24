064b75c2eed6dacde4f09bd4c22f6fb1863e52a4
agustn134 <agustinparralopez1@gmail.com>
2024-11-23 18:23:41 -0600

    archivo keys.ts para que utilice las variables de entorno en lugar de tener las credenciales directamente en el código

--- server/build/keys.js
+++ server/build/keys.js
@@ -1,16 +1,26 @@
 "use strict";
+// export default{
+//     database:{
+//         // host: 'localhost',
+//         // user: 'root',
+//         // password: '',
+//         // database: 'bd_psga6'
+//         host: 'sql3.freesqldatabase.com',
+//         user: 'sql3746356',
+//         password: 'Eb42zTrQXh',
+//         database: 'sql3746356',
+//         port: 3306 // Opcional si ya es el puerto predeterminado
+//     },
+//     jwtSecret: '1234567890'
 Object.defineProperty(exports, "__esModule", { value: true });
+// }
 exports.default = {
     database: {
-        // host: 'localhost',
-        // user: 'root',
-        // password: '',
-        // database: 'bd_psga6'
-        host: 'sql3.freesqldatabase.com',
-        user: 'sql3746356',
-        password: 'Eb42zTrQXh',
-        database: 'sql3746356',
-        port: 3306 // Opcional si ya es el puerto predeterminado
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASSWORD || '',
+        database: process.env.DB_NAME || 'bd_psga6',
+        port: parseInt(process.env.DB_PORT || '3306', 10),
     },
-    jwtSecret: '1234567890'
+    jwtSecret: process.env.JWT_SECRET || '1234567890',
 };
--- server/src/keys.ts
+++ server/src/keys.ts
@@ -1,15 +1,27 @@
-export default{
-    database:{
-        // host: 'localhost',
-        // user: 'root',
-        // password: '',
-        // database: 'bd_psga6'
-        host: 'sql3.freesqldatabase.com',
-        user: 'sql3746356',
-        password: 'Eb42zTrQXh',
-        database: 'sql3746356',
-        port: 3306 // Opcional si ya es el puerto predeterminado
-    },
-    jwtSecret: '1234567890'
+// export default{
+//     database:{
+//         // host: 'localhost',
+//         // user: 'root',
+//         // password: '',
+//         // database: 'bd_psga6'
+//         host: 'sql3.freesqldatabase.com',
+//         user: 'sql3746356',
+//         password: 'Eb42zTrQXh',
+//         database: 'sql3746356',
+//         port: 3306 // Opcional si ya es el puerto predeterminado
+//     },
+//     jwtSecret: '1234567890'
 
-}
+// }
+
+export default {
+    database: {
+      host: process.env.DB_HOST || 'localhost',
+      user: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_NAME || 'bd_psga6',
+      port: parseInt(process.env.DB_PORT || '3306', 10),
+    },
+    jwtSecret: process.env.JWT_SECRET || '1234567890',
+  };
+  
