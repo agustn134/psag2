5540bb98afbc071a0df37f7cde4f51ec4fd4cb9a
agustn134 <agustinparralopez1@gmail.com>
2025-01-23 20:43:22 -0600

    Cambio a local

--- server/build/keys.js
+++ server/build/keys.js
@@ -1,26 +1,26 @@
 "use strict";
-// export default{
-//     database:{
-//         // host: 'localhost',
-//         // user: 'root',
-//         // password: '',
-//         // database: 'bd_psga6'
-//         host: 'sql3.freesqldatabase.com',
-//         user: 'sql3746356',
-//         password: 'Eb42zTrQXh',
-//         database: 'sql3746356',
-//         port: 3306 // Opcional si ya es el puerto predeterminado
-//     },
-//     jwtSecret: '1234567890'dfgdfgfd
 Object.defineProperty(exports, "__esModule", { value: true });
-// }
 exports.default = {
     database: {
-        host: process.env.DB_HOST || 'sql5.freesqldatabase.com',
-        user: process.env.DB_USER || 'sql5747655',
-        password: process.env.DB_PASSWORD || '7ZE4ZbGWph',
-        database: process.env.DB_NAME || 'sql5747655',
-        port: parseInt(process.env.DB_PORT || '3306', 10),
+        // host: 'localhost',
+        // user: 'root',
+        // password: '',
+        // database: 'bd_psga6'
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'bd_psag6',
+        port: 3306 // Opcional si ya es el puerto predeterminado
     },
-    jwtSecret: process.env.JWT_SECRET || '1234567890',
+    jwtSecret: '1234567890'
 };
+// export default {
+//     database: {
+//       host: process.env.DB_HOST || 'sql5.freesqldatabase.com',
+//       user: process.env.DB_USER || 'sql5747655',
+//       password: process.env.DB_PASSWORD || '7ZE4ZbGWph',
+//       database: process.env.DB_NAME || 'sql5747655',
+//       port: parseInt(process.env.DB_PORT || '3306', 10),
+//     },
+//     jwtSecret: process.env.JWT_SECRET || '1234567890',
+//   };
